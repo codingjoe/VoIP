bf1bed6977880b3629f87dfaa44c78b4842551f7
Johannes Maron <johannes@maron.family>
2026-03-02 15:08:13 +0100

    Drop top level imports

--- sip/__init__.py
+++ sip/__init__.py
@@ -1,10 +1,7 @@
 """Python asyncio library for the Session Initiation Protocol (SIP)."""
 
 from . import _version
-from .messages import Request, Response, SIPMessage
-from .aio import SIP, SessionInitiationProtocol
 
 __version__ = _version.version
 VERSION = _version.version_tuple
 
-__all__ = ["Request", "Response", "SIP", "SIPMessage", "SessionInitiationProtocol"]
